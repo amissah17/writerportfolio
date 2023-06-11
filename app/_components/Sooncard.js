12c6e6c6da06fa191ae5187ad5ec2f240fc9e234
amissah17 <amissahfrank17@gmail.com>
2023-06-11 10:45:17 -0700

    fix:build error

---
+++ app/_components/Sooncard.js
@@ -0,0 +1,11 @@
+import style from './Sooncard.module.css'
+
+function Soon() {
+    return ( 
+        <div className={style.wrapper}>
+            <p>Coming soon</p>
+        </div>
+     );
+}
+
+export default Soon;
